9218c6abc73655ce29b9760c7ec52495a50202ae
danmarshall208 <danmarshall208@hotmail.com>
2016-10-24 16:36:54 +1000

    Added inbuildings asset list request

--- medusa.py
+++ medusa.py
@@ -8,14 +8,19 @@ import requests
 
 app = Flask(__name__)
 binds = {
-    'sbo':  'mssql+pyodbc://admin:password@127.0.0.1\SQLEXPRESS/StruxureWareReportsDB?driver=SQL+Server+Native+Client+10.0'
+    'sbo':      'mssql+pyodbc://admin:password@127.0.0.1\SQLEXPRESS/StruxureWareReportsDB?driver=SQL+Server+Native+Client+10.0',
+    'medusa':   'mssql+pyodbc://admin:password@127.0.0.1\SQLEXPRESS/Medusa?driver=SQL+Server+Native+Client+10.0'
 }
 app.config['SQLALCHEMY_BINDS'] = binds
-app.config['SECRET_KEY'] = "random string"
+app.config['SECRET_KEY'] = 'random string'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = True
 db = SQLAlchemy(app)
 api = Api(app)
+
+##
+## class definitions
+##
 
 # table with trend log values
 class LogTimeValues(db.Model):
@@ -46,32 +51,112 @@ class LoggedEntities(db.Model):
     meter_startpoint = db.Column('MeterStartPoint',db.Float)
     last_read_value = db.Column('LastReadValue',db.Float)
 
-#return a value from a trendlog	
+# asset list
+class Assets(db.Model):
+    __bind_key__ = 'medusa'
+    eid = db.Column('EID',db.Integer,primary_key=True)
+    name = db.Column('Name',db.String(512))
+    location = db.Column('Location',db.String(512))
+    group = db.Column('Group',db.String(512))
+
+    def __init__(self,eid,name,group,location):
+        self.eid = eid
+        self.name = name
+        self.location = location
+        self.group = group
+        
+
+##
+## app.route functions
+##
+
+# return a value from a trendlog	
 @app.route('/<string:logname>/<int:seqno>')
 def sql_test(name,seqno):
     trendlog_entity = LoggedEntities.query.filter(LoggedEntities.path.like('%' + logname)).first()
-    trend_value = LogTimeValues.query.filter_by(parent_id==trendlog_entity.entity_id, seqno==seqno).first().float_value
+    trend_value = LogTimeValues.query.filter_by(parent_id == trendlog_entity.entity_id, seqno == seqno).first().float_value
     return str(trend_value)
 
-#check comms with inbuildings server
+# check comms with inbuildings server
 @app.route('/inbuildings')
 def inbuildings_test():
     # setup request parameters
-    key = "0P8q1M8x8k1K4m7t8H2g5E1d8d5A4h3e1h2d9J3U3R7h2V9q9L6R6x8n9W4l3K9o6F1e8e6N7g4w7d1B2T1C6K9u6H9I4Y9b6J3m3z5I7q7b1e2q8p1z2R9K5I1f3P1I1o6f9u7v9b1Z2s4h8D1B8o9C7N5y5Y9N8I2T5i3W9o5e9c3F5K4j2u2y9k9r4j1Y9E1w4f6s6"
-    url = "https://inbuildings.info/ingenius/rest/sbo.php"
-    headers = {'content-type': 'application/x-www-urlencoded'}
-    message = "Comms Test"
-    mode = "raisenewjob"
+    key = '0P8q1M8x8k1K4m7t8H2g5E1d8d5A4h3e1h2d9J3U3R7h2V9q9L6R6x8n9W4l3K9o6F1e8e6N7g4w7d1B2T1C6K9u6H9I4Y9b6J3m3z5I7q7b1e2q8p1z2R9K5I1f3P1I1o6f9u7v9b1Z2s4h8D1B8o9C7N5y5Y9N8I2T5i3W9o5e9c3F5K4j2u2y9k9r4j1Y9E1w4f6s6'
+    message = 'Comms Test'
+    mode = 'raisenewjob'
+    test = 'yes'
+    equip_id = '0'
+    priority_id = '7'
+    data = {'key':key, 'mode': mode, 'test': test, 'eid': equip_id, 'pid': priority_id, 'body': message}
 
     # send request
     try:
-        resp = requests.post(url, data = {'key':key, 'mode': mode, 'test': 'yes', 'eid': '0', 'pid': '7', 'body': message}, headers = headers)
-        # parse server response into readable type
-        resp_parsed = resp.json()
-    except:
+        resp = inbuildings_request(data)
+    except requests.exceptions.ConnectionError:
         return "No Comms"
     else:
         return "Comms OK"
 
+# run checks
+@app.route('/check/AHU/4')
+def airtemp_heating_check():
+    return str(True)
+
+@app.route('/check/AHU/8')
+def simult_heatcool_check():
+    return str(True)
+
+@app.route('/check/AHU/13')
+def fan_unoccupied_check():
+    return str(True)
+
+@app.route('/check/AHU/14')
+def ahu_occupied_check():
+    return str(False)
+
+@app.route('/check/AHU/16')
+def chw_hunting_check():
+    return str(False)
+
+@app.route('/check/AHU/19')
+def run_hours_check():
+    return str(False)
+
+
+##
+##  non app.route functions
+##
+
+# generic inbuildings request
+def inbuildings_request(data):
+    # setup request parameters
+    url = 'https://inbuildings.info/ingenius/rest/sbo.php'
+    headers = {'content-type': 'application/x-www-urlencoded'}
+    resp = requests.post(url, data = data, headers = headers)
+    # parse server response into readable type
+    resp_parsed = resp.json()
+    return resp_parsed
+
+# inbuildings asset request
+def inbuildings_asset_request():
+    # setup request parameters
+    key = '0P8q1M8x8k1K4m7t8H2g5E1d8d5A4h3e1h2d9J3U3R7h2V9q9L6R6x8n9W4l3K9o6F1e8e6N7g4w7d1B2T1C6K9u6H9I4Y9b6J3m3z5I7q7b1e2q8p1z2R9K5I1f3P1I1o6f9u7v9b1Z2s4h8D1B8o9C7N5y5Y9N8I2T5i3W9o5e9c3F5K4j2u2y9k9r4j1Y9E1w4f6s6'
+    mode = "equipmentlist"
+    data = {'key':key, 'mode': mode}
+    resp = inbuildings_request(data)
+    
+    for asset in resp:
+        if Assets.query.filter_by(eid=asset['eid']).first() is None:
+            db_asset = Assets(asset['eid'],asset['name'],asset['location'],asset['group'])
+        else:
+            db_asset = Assets.query.filter_by(eid=asset['eid']).first()
+            db_asset.name = asset['name']
+            db_asset.location = asset['location']
+            db_asset.group = asset['group']
+        db.session.add(db_asset)
+        
+    db.session.commit()
+
 if __name__ == '__main__':
+    db.create_all(bind='medusa')
     app.run(debug=True)
