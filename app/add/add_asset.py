001a97ec43023afba4e21bb698bde58c4fc53b24
Daniel Marshall <SESA388003@schneider-electric.com>
2017-04-06 16:43:48 +1000

    Fixed excel upload
    Added upload confirmation screen

--- app/add/add_asset.py
+++ app/add/add_asset.py
@@ -9,7 +9,7 @@ from io import BytesIO
 from xml.etree.ElementTree import ElementTree, Element, SubElement
 
 # page to add an asset to the site
-@app.route('/site/<sitename>/add_asset')
+@app.route('/site/<sitename>/add')
 def add_asset_input(sitename):
     site = Site.query.filter_by(name=sitename).one()
     asset_types = AssetType.query.all()
@@ -112,33 +112,24 @@ def add_asset_download(sitename):
     ws_input['B1'] = "Location"
     ws_input['C1'] = "Group"
     ws_input['D1'] = "Type"
-    ws_input['E1'] = "Subtype"
-    ws_input['F1'] = "Priority"
+    ws_input['E1'] = "Priority"
 
     # generate page with all the options (for data validation)
     ws_options = wb.create_sheet("Options")
     x = 1
     for asset_type in AssetType.query.all():
         ws_options.cell(column=x, row=1).value = asset_type.name
-        y = 2
-        for subtype in asset_type.subtypes:
-            ws_options.cell(column=x, row=y).value = subtype.name
-            y += 1
         x += 1
-        wb.create_named_range(asset_type.name, ws_options, "${}$2:${}${}".format(get_column_letter(x-1), get_column_letter(x-1), y-1))
     cols = len(tuple(ws_options.columns))
     wb.create_named_range("Types", ws_options, '$A$1:${}$1'.format(get_column_letter(cols)))
 
     # apply data validation to input page
     type_dv = DataValidation(type='list', formula1='Types', allow_blank=True)
-    subtype_dv = DataValidation(type='list', formula1='INDIRECT(INDIRECT(ADDRESS(ROW(), COLUMN()-1)))', allow_blank=True)
     priority_dv = DataValidation(type='whole', operator='between', formula1=0, formula2=9)
     ws_input.add_data_validation(type_dv)
-    ws_input.add_data_validation(subtype_dv)
     ws_input.add_data_validation(priority_dv)
     type_dv.ranges.append('D2:D1000')
-    subtype_dv.ranges.append('E2:E1000')
-    priority_dv.ranges.append('F2:F1000')
+    priority_dv.ranges.append('E2:E1000')
 
     # save file
     out = BytesIO(save_virtual_workbook(wb))
@@ -149,14 +140,14 @@ def add_asset_download(sitename):
     return response
 
 # process an asset addition via upload
-@app.route('/site/<sitename>/add/_upload', methods=['POST'])
+@app.route('/site/<sitename>/add/confirm', methods=['POST'])
 def add_asset_upload(sitename):
 
     site = Site.query.filter_by(name=sitename).one()
 
     # check if file was uploaded
     if 'file' not in request.files:
-        flash('No file part')
+        flash('No file selected')
         return redirect(url_for('add_asset_input', sitename=sitename))
     file = request.files['file']
     if file.filename =='':
@@ -174,7 +165,8 @@ def add_asset_upload(sitename):
     # generate asset for each non-blank row in the worksheet
     asset_list = []
     for row in tuple(ws.rows)[1:]:
-        if not row[0].value is None and not row[3].value is None and not row[4].value is None and not row[5].value is None:
+        # skip if not all required information is present
+        if not row[0].value is None and not row[3].value is None and not row[4].value is None:
             name = row[0].value
             location = row[1].value
             if location is None:
@@ -183,46 +175,107 @@ def add_asset_upload(sitename):
             if group is None:
                 group = ""
             type_name = row[3].value
-            subtype_name = row[4].value
-            priority = row[5].value
+            priority = row[4].value
             asset_type = AssetType.query.filter_by(name=type_name).one()
-            subtype = FunctionalDescriptor.query.filter_by(name=subtype_name, type=asset_type).one()
-            asset = Asset(name=name, location=location, group=group, subtype=subtype, priority=priority, site=site, health=0)
-
-            db.session.add(asset)
+            asset = Asset(name=name, location=location, group=group, type=asset_type, priority=priority)
             asset_list.append(asset)
 
-    # generate points
-    for subtype_point in subtype.points:
-        point = AssetPoint(type=subtype_point.type, asset=asset, name=subtype_point.type.name)
+    return render_template('add_asset_confirm.html', assets=asset_list, site=site)
 
-        # set destination folder
-        point.loggedentity_path = "/Server 1/Medusa/Trends/{}/{} Extended".format(asset.name, point.name)
-
-        db.session.add(point)
-
+# JSON confirmation for an asset upload
+# redirection after form submit occurs from within the HTML form
+# this function is only triggered by a javascript AJAX POST request (easiest way to post a JSON object to group data together)
+# which is triggered when the submit button is clicked
+@app.route('/site/<sitename>/add/_upload_confirm', methods=['POST'])
+def add_asset_confirm(sitename):
+    site = Site.query.filter_by(name=sitename).one()
+    assets_json = request.get_json(force=True)
+    # returns a dict with each 'value' being the data for one asset, so iterate through the values
+    for asset_json in assets_json.values():
+        asset_type = AssetType.query.filter_by(name=asset_json['type']).one()
+        asset = Asset(name=asset_json['name'], location=asset_json['location'], group=asset_json['group'], type=asset_type, priority=asset_json['priority'], health=0, site=site)
+        db.session.add(asset)
+        print(asset)
     db.session.commit()
 
-    # generate xml import to SBO
-    object_set = Element('ObjectSet')
-    object_set.attrib = {'ExportMode':'Standard', 'Version':'1.8.1.87', 'Note':'TypesFirst'}
-    exported_objects = SubElement(object_set, 'ExportedObjects')
-    for asset in asset_list:
-        oi_folder = SubElement(exported_objects, 'OI')
-        oi_folder.attrib = {'NAME':asset.name, 'TYPE':'system.base.Folder'}
-        for point in asset.points:
-            oi_trend = SubElement(oi_folder, 'OI')
-            oi_trend.attrib = {'NAME':'{} Extended'.format(point.name), 'TYPE':'trend.ETLog'}
-            pi = SubElement(oi_trend, 'PI')
-            pi.attrib = {'Name':'IncludeInReports', 'Value':'1'}
 
-    # save file
-    out = BytesIO()
-    tree = ElementTree(object_set)
-    tree.write(out, encoding='utf-8', xml_declaration=True)
-    out.seek(0)
+# OLD CODE FOR GENERATING XML IMPORT - LEFT HERE TO BE REUSED LATER
 
-    # prevent browser from caching the download
-    response = make_response(send_file(out, attachment_filename='Import.xml', as_attachment=True))
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    return response
+# # process an asset addition via upload
+# @app.route('/site/<sitename>/add/_upload', methods=['POST'])
+# def add_asset_upload(sitename):
+
+#     site = Site.query.filter_by(name=sitename).one()
+
+#     # check if file was uploaded
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return redirect(url_for('add_asset_input', sitename=sitename))
+#     file = request.files['file']
+#     if file.filename =='':
+#         flash('No file selected')
+#         return redirect(url_for('add_asset_input', sitename=sitename))
+
+#     # check if file is correct type
+#     if not (file and ('.' in file.filename) and (file.filename.rsplit('.',1)[1].lower() in ['xlsx','xlsm'])):
+#         flash('File must be xlsx/xlsm')
+#         return redirect(url_for('add_asset_input', sitename=sitename))
+
+#     wb = load_workbook(file)
+#     ws = wb.worksheets[0]
+
+#     # generate asset for each non-blank row in the worksheet
+#     asset_list = []
+#     for row in tuple(ws.rows)[1:]:
+#         if not row[0].value is None and not row[3].value is None and not row[4].value is None and not row[5].value is None:
+#             name = row[0].value
+#             location = row[1].value
+#             if location is None:
+#                 location = ""
+#             group = row[2].value
+#             if group is None:
+#                 group = ""
+#             type_name = row[3].value
+#             subtype_name = row[4].value
+#             priority = row[5].value
+#             asset_type = AssetType.query.filter_by(name=type_name).one()
+#             subtype = FunctionalDescriptor.query.filter_by(name=subtype_name, type=asset_type).one()
+#             asset = Asset(name=name, location=location, group=group, subtype=subtype, priority=priority, site=site, health=0)
+
+#             db.session.add(asset)
+#             asset_list.append(asset)
+
+#     # generate points
+#     for subtype_point in subtype.points:
+#         point = AssetPoint(type=subtype_point.type, asset=asset, name=subtype_point.type.name)
+
+#         # set destination folder
+#         point.loggedentity_path = "/Server 1/Medusa/Trends/{}/{} Extended".format(asset.name, point.name)
+
+#         db.session.add(point)
+
+#     db.session.commit()
+
+#     # generate xml import to SBO
+#     object_set = Element('ObjectSet')
+#     object_set.attrib = {'ExportMode':'Standard', 'Version':'1.8.1.87', 'Note':'TypesFirst'}
+#     exported_objects = SubElement(object_set, 'ExportedObjects')
+#     for asset in asset_list:
+#         oi_folder = SubElement(exported_objects, 'OI')
+#         oi_folder.attrib = {'NAME':asset.name, 'TYPE':'system.base.Folder'}
+#         for point in asset.points:
+#             oi_trend = SubElement(oi_folder, 'OI')
+#             oi_trend.attrib = {'NAME':'{} Extended'.format(point.name), 'TYPE':'trend.ETLog'}
+#             pi = SubElement(oi_trend, 'PI')
+#             pi.attrib = {'Name':'IncludeInReports', 'Value':'1'}
+
+#     # save file
+#     out = BytesIO()
+#     tree = ElementTree(object_set)
+#     tree.write(out, encoding='utf-8', xml_declaration=True)
+#     out.seek(0)
+
+#     # prevent browser from caching the download
+#     response = make_response(send_file(out, attachment_filename='Import.xml', as_attachment=True))
+#     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+#     return response
