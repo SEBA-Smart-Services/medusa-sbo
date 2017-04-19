e6c4fa705a4c332c67eef186f073be6298e31d00
Daniel Marshall <SESA388003@schneider-electric.com>
2017-04-19 13:47:17 +1000

    added database regeneration file

--- medusa.py
+++ medusa.py
@@ -1,14 +1,6 @@
 # from init file
-from app import app, db, scheduler
-# packages
-from app import add, admin, cmms, weather, algorithms
-# modules
-from app import models, mapping, controllers
-
+from app import app, scheduler
 import os
-
-# generate any tables in the database that are missing, based on models
-db.create_all(bind='medusa')
 
 # prevents scheduler from running twice - flask uses 2 instances in debug mode
 if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
---
+++ regenerate.py
@@ -0,0 +1,32 @@
+from app import db
+from app.models import AssetType, PointType, Asset, Site, AssetPoint
+
+# regenerate all tables in the database, based on models
+db.drop_all(bind='medusa')
+db.create_all(bind='medusa')
+
+for sitename in ['TestSite1', 'TestSite2']:
+    site = Site(name=sitename)
+    db.session.add(site)
+
+for typename in ['AHU', 'Chiller']:
+    asset_type = AssetType(name=typename)
+    db.session.add(asset_type)
+
+for pointname in ['Fan Enable', 'CHW Valve']:
+    point_type = PointType(name=pointname)
+    db.session.add(point_type)
+
+site = Site.query.filter_by(name='TestSite1').one()
+asset_type = AssetType.query.filter_by(name='AHU').one()
+asset = Asset(name='Test AHU', site=site, type=asset_type, health=0, priority=1, notes='')
+point1_type = PointType.query.filter_by(name='Fan Enable').one()
+point2_type = PointType.query.filter_by(name='CHW Valve').one()
+point1 = AssetPoint(name='Fan Enable', type=point1_type, asset=asset)
+point2 = AssetPoint(name='CHW Valve', type=point2_type, asset=asset)
+asset.points.append(point1)
+asset.points.append(point2)
+
+db.session.add(asset)
+db.session.commit()
+db.session.close()
