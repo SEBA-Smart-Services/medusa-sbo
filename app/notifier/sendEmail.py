2bb7183d2fdf1d95e83994f809cb83ea3e7f8d69
Christopher VASILIOU <SESA392626@schneider-electric.com>
2017-04-28 08:54:11 +1000

    edit sendemail.py

--- app/notifier/sendEmail.py
+++ app/notifier/sendEmail.py
@@ -1,14 +1,13 @@
 import smtplib
 
-user = "AKIAIFZ67S3NUJ6HEOEQ"
-pw = "AvwPSekE6tpVsK22XAh+3Cat1dqDiGaKIOJQk31ZReUO"
+user = ""
+pw = ""
 host = "email-smtp.us-west-2.amazonaws.com"
 port = 587
 me   = "medusa@sebbqld.com"
 you  = ("christopher.vasiliou@schneider-electric.com",)
-body = "Test"
+body = "The gorgon Medusa has risen!! \n Stare into my eyes!"
 msg  = ("From: %s\r\nTo: %s\r\n\r\n" % (me, ", ".join(you)))
-
 msg = msg + body
 s = smtplib.SMTP(host, port, timeout = 10)
 s.starttls()
