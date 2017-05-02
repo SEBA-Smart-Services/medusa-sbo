855227047f9deb91cd5e7a2b7d0671b1321dc585
Daniel Marshall <SESA388003@schneider-electric.com>
2017-05-02 18:45:50 +1000

    added pdf report

--- app/reports/__init__.py
+++ app/reports/__init__.py
@@ -1,1 +1,4 @@
 from . import controllers
+import logging
+# silence weasyprint warning output
+logging.getLogger('weasyprint').setLevel(100)
--- app/reports/controllers.py
+++ app/reports/controllers.py
@@ -1,7 +1,7 @@
 from app import app
 from app.models import Site, Asset, Result
 from flask import render_template
-#from weasyprint import HTML
+from flask_weasyprint import HTML, render_pdf
 
 # generate report for a site
 @app.route('/site/<sitename>/report')
@@ -13,7 +13,5 @@ def report(sitename):
     for asset in assets:
         recent_results[asset] = Result.query.filter_by(asset=asset, recent=True).all()
         unresolved_results[asset] = Result.query.filter(Result.asset==asset, (Result.active == True) | (Result.acknowledged == False)).all()
-    document_as_string = render_template('report.html', assets=assets, site=site, recent_results=recent_results, unresolved_results=unresolved_results)
-    document = HTML(string=document_as_string)
-    pdf = document.write_pdf()
-    return(pdf)
+    html = render_template('report.html', assets=assets, site=site, recent_results=recent_results, unresolved_results=unresolved_results)
+    return render_pdf(HTML(string=html))
