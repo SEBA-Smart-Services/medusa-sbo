f466937322e44ee40b754768c02def9b27e8e9f1
Clive <clive.gross1@gmail.com>
2017-05-09 17:41:21 +1000

    Update event_handler.py

--- app/event_handler/event_handler.py
+++ app/event_handler/event_handler.py
@@ -7,13 +7,13 @@ class EventHandler():
         self.notifier = Notifier()
 
     def handle_result(self, result):
-        if result.passed == False and self.test_priority(result) and result.occurances == 1:
+        if self.test_for_notification(result):
             self.notifier.send_issue(result)
             
     def test_priority(self, result):
         return result.priority < max(result.asset.site.email_trigger_priority, result.asset.site.cmms_trigger_priority)
     
-    def test_priority(self, result):
+    def test_for_notification(self, result):
         """
         testing if an issue meets the requirements to trigger a notification.
         
