2b702595f7561cbb14ca74814c07d89bc8d2ba4b
SEBB <clive@schneider-electric.com>
2017-08-20 14:29:19 +1000

    tidied up site agent config page layout, fixed jquery ajax

--- app/sitedataagent/controllers.py
+++ app/sitedataagent/controllers.py
@@ -17,73 +17,40 @@ def site_data_agent(sitename):
         if form.validate_on_submit():
             return redirect(url_for('site_data_agent', sitename=sitename, form=form))
 
-        #
-        # # if form has errors, return the page (errors will display)
-        # if not form.validate_on_submit():
-        #     return render_template('site_config.html', inbuildings_config=inbuildings_config, site=site, form=form)
-        #
-        # # convert port input to a string
-        # if form.db_port.data is None:
-        #     form.db_port.data = ""
-        # form.db_port.data = str(form.db_port.data)
-        #
-        # # update webreports database settings
-        # form.populate_obj(site)
-        # site.generate_key()
-        #
-        # # update inbuildings settings
-        # #################################
-        # # There appears to be some orphan sites without InbuildingsConfig relationships
-        # # not sure how/when??
-        # # this page breaks for these sites'
-        # # this if statement prevents this
-        # if inbuildings_config is not None:
-        #     inbuildings_config.enabled = form.inbuildings_enabled.data
-        #     inbuildings_config.key = form.inbuildings_key.data
-        #
-        # # update emails
-        # emails = []
-        # # remove whitespace and separate out emails from csv
-        # email_strings = set(form.email_list.data.replace(" ", "").split(','))
-        # for email_string in email_strings:
-        #     if email_string != '':
-        #         emails.append(Email(address=email_string))
-        # site.emails = emails
-        #
-        # db.session.commit()
+
         return 'little success ' + site.name# redirect(url_for('site_data_agent', sitename=sitename))
 
     elif request.method == 'GET':
-        # # prefill form with information from site object
-        # form = SiteConfigForm(obj=site)
-        #
-        # #################################
-        # # There appears to be some orphan sites without InbuildingsConfig relationships
-        # # not sure how/when??
-        # # this page breaks for these sites'
-        # # this if statement prevents this
-        # if inbuildings_config is None:
-        #     form.inbuildings_enabled.data = False # inbuildings_config.enabled
-        #     form.inbuildings_key.data = ''# inbuildings_config.key
-        # else:
-        #     form.inbuildings_enabled.data = inbuildings_config.enabled
-        #     form.inbuildings_key.data = inbuildings_config.key
-        # # turn emails into csv
-        # form.email_list.data = ','.join([email.address for email in site.emails])
-        return render_template('site_settings/site_data_agent.html', site=site, form=form)
+
+        return render_template('site_settings/site_data_agent.html', site=site, is_enabled="not working yet")
 
 
-@app.route('/_generate_aws_access_keys/<int:site_id>')
+@app.route('/_generate_access_keys/<int:site_id>')
 def add_numbers(site_id):
-    print(site_id)
+    # create IAM object from AWS utils
     iam = AwsIamManager(site_id)
+    # get enabled state of site data agent from client over ajax
+    is_enabled_ajax = request.args.get('isChecked', 0, type=int)
+    ########
+    # SECURITY: need to protect this endpoint with a pre-generated token
+    ########
     resp = iam.delete_access_key()
-    delete_resp = iam.delete_user()
-    create_resp = iam.create_user()
-    resp = iam.create_access_key()
-    resp = iam.delete_access_key()
-    delete_resp = iam.delete_user()
+    resp = iam.delete_user()
+    if is_enabled_ajax == 0:
+        is_enabled = False
+        access_key_id = str(None)
+        secret_access_key = str(None)
+    else:
+        is_enabled = True
+        resp = iam.create_user()
+        resp = iam.create_access_key()
+        access_key_id = iam.access_key_id
+        secret_access_key = iam.secret_access_key
+        resp = iam.delete_access_key()
+        delete_resp = iam.delete_user()
+
     return jsonify(
-        accessKeyId=iam.access_key_id,
-        secretAccessKey=iam.secret_access_key
+        accessKeyId=access_key_id,
+        secretAccessKey=secret_access_key,
+        isEnabled=str(is_enabled)
     )
