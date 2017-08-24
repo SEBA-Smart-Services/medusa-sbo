66b403d2fd0f85ace8ad47b4d2028f33fa202e20
SEBB <clive@schneider-electric.com>
2017-08-25 00:21:48 +1000

    asset detail page show point trend logs

--- app/hvac_assets/controllers/ajax.py
+++ app/hvac_assets/controllers/ajax.py
@@ -1,12 +1,19 @@
+from flask import jsonify
 from app import app, db
-from app.models import Site
+from app.models import AssetPoint, LoggedEntity, LogTimeValue
+from app.hvac_assets.utils import LogSampleFetcher
 
 @app.route('/_pointdata/<int:asset_point_id>')
 def get_pointdata(asset_point_id):
     """
     returns asset point log samples
     """
+    data_fetcher = LogSampleFetcher(asset_point_id)
+    samples = data_fetcher.range(as_list=True)
 
+    app.logger.info(str(samples))
+    
     return jsonify(
-        testKey="penis"
+        testKey="penis",
+        samples=samples
     )
---
+++ app/hvac_assets/utils/__init__.py
@@ -0,0 +1,1 @@
+from .log_sample import LogSampleFetcher
